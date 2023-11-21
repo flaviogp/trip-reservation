03a2afe8c714daa264db22c1385f76269157a503
FlavioGP <flavio.gpinheiro@hotmail.com>
2023-11-21 11:05:09 -0300

    feat: add recommended trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 
-import QuickSearch from "./components/QuickSearch"
-import TripSearch from "./components/TripSearch"
+import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
+import TripSearch from "./components/TripSearch";
 
 export default function Home() {
-
-  return ( 
+  return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
-  )
+  );
 }
